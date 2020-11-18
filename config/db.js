e3303289d7e86efaf7e6210ab9b28e42deb9f927
alyson-b69 <alyson.bernabeu@gmail.com>
2020-11-18 22:02:34 +0000

    add some changes for deploy on VPS

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
-const db = require("mysql");
+const db = require("mariadb");
 const dotenv = require("dotenv").config();
 module.exports = db.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,  
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  host: process.env.MYSQL_HOST,
   database: process.env.MYSQL_DATABASE,
 });
