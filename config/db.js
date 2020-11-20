fd5cd0c18c00ccd99d9b2df484d7ed3cb46c6f90
alyson-b69 <alyson.bernabeu@gmail.com>
2020-11-20 01:33:59 +0100

    change mariadb for mysql

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-const db = require("mariadb");
+const db = require("mysql");
 const dotenv = require("dotenv").config();
 module.exports = db.createConnection({
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,  
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
